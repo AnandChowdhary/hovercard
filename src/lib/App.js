fc3c6a35b3f140b3dd45056c6d4591d7ceee3772
Anand Chowdhary <anandchowdhary@gmail.com>
2018-12-04 20:56:22 +0100

    Better keep showing

--- src/lib/App.js
+++ src/lib/App.js
@@ -7,12 +7,12 @@ const hasMouseOver = domElement => domElement && domElement.parentElement.queryS
 
 class Hovercard {
   constructor(settings = {}) {
-    this.elements = document.querySelectorAll(".hovercard");
     this.setup();
-    this.padding = 0;
+    this.padding = 20;
     this.settings = settings;
   }
   setup() {
+    this.elements = document.querySelectorAll(".hovercard");
     for (let i = 0; i < this.elements.length; i++) {
       this.elements[i].addEventListener("mouseover", () => this.mouseOver(this.elements[i]));
       this.elements[i].addEventListener("mouseout", () => this.mouseOut(this.elements[i]));
@@ -22,11 +22,11 @@ class Hovercard {
     if (document.querySelector(".hovercard-element")) return;
     const card = document.createElement("div");
     card.classList.add("hovercard-element");
-    card.innerHTML = `<div class="hovercard-card"></div>`;
     document.body.appendChild(card);
     card.addEventListener("mouseout", () => this.mouseOut());
     const arrow = document.createElement("div");
     arrow.classList.add("hovercard-arrow");
+    arrow.addEventListener("mouseout", () => this.mouseOut());
     document.body.appendChild(arrow);
   }
   positionHovercard(position) {
@@ -34,20 +34,21 @@ class Hovercard {
     const arrow = document.querySelector(".hovercard-arrow"); if (!arrow) return;
     const scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
     const scrollLeft = (window.pageXOffset !== undefined) ? window.pageXOffset : (document.documentElement || document.body.parentNode || document.body).scrollLeft;
-    card.style.top = (scrollTop + position.top + position.height + this.padding - 10) + "px";
+    card.style.top = (scrollTop + position.top + position.height + this.padding) + "px";
     card.style.left = (scrollLeft + position.left) + "px";
-    arrow.style.top = (scrollTop + position.top + position.height + this.padding + 10) + "px";
-    arrow.style.left = (scrollLeft + position.left + (position.width / 2) - 5) + "px";
+    arrow.style.top = (scrollTop + position.top + position.height + this.padding - 10) + "px";
+    arrow.style.left = (scrollLeft + position.left) + "px";
+    arrow.style.paddingLeft = ((position.width / 2) - 5) + "px";
   }
   updateHovercard(data) {
     if (!(data.displaytitle && data.extract)) return;
     const card = document.querySelector(".hovercard-element"); if (!card) return;
     const arrow = document.querySelector(".hovercard-arrow"); if (!arrow) return;
-    card.querySelector(".hovercard-card").innerHTML = `
+    card.innerHTML = `
       <h2 class="hovercard-title">${data.displaytitle}</h2>
       <p class="hovercard-description">${data.extract}</p>`;
     if (data.thumbnail && data.thumbnail.source) {
-      card.querySelector(".hovercard-card").innerHTML += `<div class="hovercard-image" style="background-image: url('${data.thumbnail.source}')"></div>`;
+      card.innerHTML += `<div class="hovercard-image" style="background-image: url('${data.thumbnail.source}')"></div>`;
       card.classList.add("hovercard-has-image");
     } else {
       card.classList.remove("hovercard-has-image");
@@ -77,12 +78,15 @@ class Hovercard {
     setTimeout(() => {
       const card = document.querySelector(".hovercard-element"); if (!card) return;
       const arrow = document.querySelector(".hovercard-arrow"); if (!arrow) return;
-      if (!hasMouseOver(card)) {
+      let hasHover = hasMouseOver(card) || hasMouseOver(arrow);
+      for (let i = 0; i < this.elements.length; i++)
+        if (hasMouseOver(this.elements[i])) hasHover = true;
+      if (!hasHover) {
         element && element.classList.remove("hovercard-visible");
         card.classList.remove("hovercard-visible");
         arrow.classList.remove("hovercard-visible");
       }
-    }, this.settings.timeout || 10);
+    }, this.settings.timeout || 100);
   }
 }
 
